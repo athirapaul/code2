8a19e1faf09c538f083236dff896984282ce782f
athirapaul <athirapaul24@gmail.com>
2019-02-14 11:58:43 -0500

    Initial Commit

---
+++ code2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  code2
+//
+//  Created by Athira Paul on 2019-02-14.
+//  Copyright © 2019 Athira Paul. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
